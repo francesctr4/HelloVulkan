4101ace3fd5ac00d237a5e55fdf4ba301c833491
Francesc Teruel Rodríguez <francesc.tr03@gmail.com>
2024-01-21 18:23:14 +0100

    Reorganizing code

--- Vulkan Integration/Source/HelloVulkan.h
+++ Vulkan Integration/Source/HelloVulkan.h
@@ -16,6 +16,8 @@
 #include "Vulkan.h"
 #include "MathGeoLib.h"
 
+const int MAX_FRAMES_IN_FLIGHT = 2;
+
 const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
 const std::vector<const char*> deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
 
@@ -33,55 +35,6 @@ VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const
 void DestroyDebugUtilsMessengerEXT(VkInstance instance,
 	VkDebugUtilsMessengerEXT debugMessenger, const
 	VkAllocationCallbacks* pAllocator);
-
-struct Vertex {
-
-	float2 position;
-	float3 color;
-
-	static VkVertexInputBindingDescription getBindingDescription() {
-
-		VkVertexInputBindingDescription bindingDescription{};
-		
-		bindingDescription.binding = 0;
-		bindingDescription.stride = sizeof(Vertex);
-		bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-
-		return bindingDescription;
-
-	}
-
-	static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
-
-		std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
-		
-		// Position
-
-		attributeDescriptions[0].binding = 0;
-		attributeDescriptions[0].location = 0;
-		attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
-		attributeDescriptions[0].offset = offsetof(Vertex, position);
-
-		// Color
-
-		attributeDescriptions[1].binding = 0;
-		attributeDescriptions[1].location = 1;
-		attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-		attributeDescriptions[1].offset = offsetof(Vertex, color);
-
-		return attributeDescriptions;
-		
-	}
-
-};
-
-//const std::vector<Vertex> vertices = {
-//
-//	{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-//	{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-//	{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
-//
-//};
 
 struct QueueFamilyIndices {
 
@@ -104,7 +57,54 @@ struct SwapChainSupportDetails {
 
 };
 
-const int MAX_FRAMES_IN_FLIGHT = 2;
+struct Vertex {
+
+	float2 position;
+	float3 color;
+
+	static VkVertexInputBindingDescription getBindingDescription() {
+
+		VkVertexInputBindingDescription bindingDescription{};
+
+		bindingDescription.binding = 0;
+		bindingDescription.stride = sizeof(Vertex);
+		bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+		return bindingDescription;
+
+	}
+
+	static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+
+		std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+
+		// Position
+
+		attributeDescriptions[0].binding = 0;
+		attributeDescriptions[0].location = 0;
+		attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+		attributeDescriptions[0].offset = offsetof(Vertex, position);
+
+		// Color
+
+		attributeDescriptions[1].binding = 0;
+		attributeDescriptions[1].location = 1;
+		attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+		attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+		return attributeDescriptions;
+
+	}
+
+};
+
+//const std::vector<Vertex> vertices = {
+//
+//	{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+//	{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+//	{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+//
+//};
 
 class HelloVulkan {
 public:
