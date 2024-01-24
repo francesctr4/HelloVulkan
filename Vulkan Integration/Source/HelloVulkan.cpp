f0f3e8d1abf9bfa04f25d5c73337744a5e3867d3
Francesc Teruel Rodríguez <francesc.tr03@gmail.com>
2024-01-24 18:33:01 +0100

    UpdateUniformBuffer() minor changes

--- Vulkan Integration/Source/HelloVulkan.cpp
+++ Vulkan Integration/Source/HelloVulkan.cpp
@@ -1049,10 +1049,13 @@ void HelloVulkan::UpdateUniformBuffer(uint32_t currentImage)
     float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 
     UniformBufferObject UBO{};
-    UBO.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-    UBO.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+
     UBO.projection = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float)swapChainExtent.height, 0.1f, 10.0f);
     UBO.projection[1][1] *= -1;
+
+    UBO.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+
+    UBO.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 
     memcpy(uniformBuffersMapped[currentImage], &UBO, sizeof(UBO));
 }
